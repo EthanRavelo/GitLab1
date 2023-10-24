db3ee00620de4feeabe04e3d7a78e0a94ec6d9db
Ethan Ravelo <ethan.ravelo02@gmail.com>
2023-10-24 16:59:21 -0700

    edited one more comment for consistency

--- process_movie_data.py
+++ process_movie_data.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-# A script that imports movie data and finds the top-5 highest grossing movies
+# A script that imports movie data and finds the top-n highest grossing movies
 import csv
 
 
